7e818b143d519753868882ec0f2855ab29607805
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-02-24 14:35:59 +0100

    WIP Header

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+    <title>php-google-faq</title>
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+        <img src="img/logo.png" alt="Google logo">
+        <h2><a href="#">Privacy e termini</a></h2>
+      </div>
+      <nav>
+      </nav>
+    </header>
+  </body>
+</html>
